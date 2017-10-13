9178d0461f81590637b7ed12c3329561191e9c9b
Adam Shanley <quartertonality@gmail.com>
2017-10-13 09:17:20 -0500

    Fizz buzz problem
    A simple program that prints the integers from 1 to 100, but for multiples of three prints "Fizz" instead of the number, and for the multiples of five prints "Buzz." For numbers which are multiples of both three and five the program prints "FizzBuzz".

---
+++ fizz-buzz.py
@@ -0,0 +1,9 @@
+for num in range(101):
+    if num % 3 == 0 and num % 5 == 0:
+        print(str(num) + " FizzBuzz")
+    elif num % 3 == 0:
+        print(str(num) + " Fizz")
+    elif num % 5 == 0:
+        print(str(num) + " Buzz")
+    else:
+        print(str(num))
